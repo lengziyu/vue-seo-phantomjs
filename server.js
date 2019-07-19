ad4da1278738bf69c9b81edd5ab3ae30a20b3346
lengziyu <1058566903@qq.com>
2019-07-19 16:55:20 +0800

    新增说明

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ var path = process.cwd();
 // 引入NodeJS的子进程模块
 var child_process = require('child_process');
 app.get('*', function(req, res){
-    // 完整URL
+    // 部署到服务器的完整URL
     var url = req.protocol + '://'+ req.hostname + req.originalUrl;
+
+    // 测试的url
+    // var url = 'https://www.baidu.com'
+    
     // 预渲染后的页面字符串容器
     var content = '';
     // 开启一个phantomjs子进程
